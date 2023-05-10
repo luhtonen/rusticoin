54577c6d5bc895559121920ac4f1f9d49c85247f
Eduard Luhtonen <luhtonen@gmail.com>
2023-05-10 13:22:23 +0300

    implement data hashing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use data_encoding::HEXUPPER;
+use ring::digest::{self, Digest};
+
 fn main() {
-    println!("Hello, world!");
+    println!("hash: {}", HEXUPPER.encode((hash("Hello, world!").as_ref())));
+}
+
+fn hash(data: &str) -> Digest {
+    digest::digest(&digest::SHA256, &data.as_bytes())
 }
