d4ad2548aa54a73158555d8c378d061574242fa6
Eduard Luhtonen <luhtonen@gmail.com>
2023-05-31 12:46:23 +0200

    add nonce and proof of work algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use std::time::SystemTime;
 #[derive(Debug)]
 pub struct Block {
     pub index: u64,
+    pub nonce: u64,
     pub data: String,
     pub current_hash: String,
     pub previous_hash: String,
@@ -12,13 +13,20 @@ pub struct Block {
 }
 
 impl Block {
-    pub fn new(index: u64, data: String, previous_hash: String) -> Block {
+    pub fn new(index: u64, data: String, previous_hash: String) -> Self {
         let timestamp = SystemTime::now();
-        let current_hash = hash(index, &data, &previous_hash, timestamp);
+        let (nonce, current_hash) = Self::proof_of_work(
+            index,
+            &data,
+            &previous_hash,
+            timestamp,
+            Some("00".to_string()),
+        );
         Block {
             index,
+            nonce,
             data,
-            current_hash: current_hash.clone(),
+            current_hash,
             previous_hash,
             timestamp,
         }
@@ -39,9 +47,36 @@ impl Block {
             previous_block.current_hash.clone(),
         )
     }
-}
-
-fn hash(index: u64, data: &str, previous_hash: &str, timestamp: SystemTime) -> String {
-    let value = format!("{}{}{}{}", index, timestamp.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis(), data, previous_hash);
-    HEXUPPER.encode(digest::digest(&digest::SHA256, value.as_bytes()).as_ref())
+    fn proof_of_work(
+        index: u64,
+        data: &str,
+        previous_hash: &str,
+        timestamp: SystemTime,
+        difficulty: Option<String>,
+    ) -> (u64, String) {
+        let difficulty = match difficulty {
+            Some(value) => value,
+            None => "00".to_string(),
+        };
+        let mut nonce = 0;
+        loop {
+            let value = format!(
+                "{}{}{}{}{}",
+                nonce,
+                index,
+                timestamp
+                    .duration_since(SystemTime::UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis(),
+                data,
+                previous_hash
+            );
+            let hash = HEXUPPER.encode(digest::digest(&digest::SHA256, value.as_bytes()).as_ref());
+            if hash.starts_with(&difficulty) {
+                return (nonce, hash);
+            } else {
+                nonce += 1;
+            }
+        }
+    }
 }
